aff0916da8f4ccbc159a8e60c5c1707922191a31
QS-SQ <samuel.inca-pilco@uni-konstanz.de>
2026-06-06 09:06:58 +0200

    evaluating host

--- scripts/methods.py
+++ scripts/methods.py
@@ -71,7 +71,7 @@ def read_email():
         mail.login(os.getenv("USER"), os.getenv("PASSWORD"))
         mail.select("INBOX")
     except Exception as e:
-        flag = f'Error connecting to email server, {e} via port {port}{type(port)}'
+        flag = f'Error connecting to email server, {e} via port {port}{type(port)} and server {server[:4]}'
         return None, None, flag
 
     # Search for emails from a specific sender that were sent in the last 24 hours
